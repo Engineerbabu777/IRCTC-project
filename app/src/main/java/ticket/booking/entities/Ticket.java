b6013f602d27d685f173ec6c0d8bd202e052327a
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-08-02 13:25:08 +0500

    first commit

---
+++ app/src/main/java/ticket/booking/entities/Ticket.java
@@ -0,0 +1,18 @@
+package ticket.booking.entities;
+
+import java.util.Date;
+
+public class Ticket {
+    
+    private String ticketid;
+
+    private String userid;
+
+    private String source;
+
+    private String destination;
+    private Date dateofTravle;
+
+    private Train train;
+
+}
